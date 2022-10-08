a337b1b9ae4294640e25ec0b810b2d94c57c29df
raul-sampietro <raul.sampietro@estudiantat.upc.edu>
2022-10-08 19:24:06 +0200

    Fixed Entity <- Player hierarchy:
    Now Player is stored as an Entity* in SceneLevel

--- R-Type/R-Type/SceneLevel.cpp
+++ R-Type/R-Type/SceneLevel.cpp
@@ -34,7 +34,6 @@ void SceneLevel::init()
 	player->init(glm::ivec2(SCREEN_X, SCREEN_Y), texProgram);
 	player->setPosition(glm::vec2(INIT_PLAYER_X_TILES * map->getTileSize(), INIT_PLAYER_Y_TILES * map->getTileSize()));
 	player->setTileMap(map);
-
 }
 
 void SceneLevel::update(int deltaTime)
--- R-Type/R-Type/SceneLevel.h
+++ R-Type/R-Type/SceneLevel.h
@@ -24,7 +24,7 @@ public:
 
 private:
 	TileMap* map;
-	Player* player;
+	Entity* player;
 
 };
 
